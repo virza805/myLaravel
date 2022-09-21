38a1f5f706fe6077b536b7f318034ac581e4ecfc
MD AL AMIN <01mdalamin1@gmail.com>
2022-09-21 13:20:32 +0600

    show slider images

--- resources/views/frontend/single.blade.php
+++ resources/views/frontend/single.blade.php
@@ -9,14 +9,22 @@
         </div>
         <div class="row">
             <div class="col-12">
+                <div class="product_img">
+                    @foreach($product->sliders as $gallery)
+                        <img style="max-width: 100%; height: 170px; width: 200px;" src="{{ asset($gallery->image) }}" alt="">
+                        {{-- <img style="max-width: 100%; height: 70px; width: 100px;" src="{{ asset('/storage/product/slider/' . $gallery->image) }}" alt=""> --}}
+                    @endforeach
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
                 <h1>{{ $product->name }}</h1>
                 <form action="">
                     <button class="btn btn-sm btn-success">Add To Cart</button>
                 </form>
                 <p>Category : {{ $product->category->name }}</p>
                 <p>Sub Category : {{ $product->sub_category->name ?? '' }}</p>
-                <p>Category : {{ $product->category->name }}</p>
-                <p>Category : {{ $product->category->name }}</p>
                 <p>{{ $product->description }}</p>
                 <hr>
                 <hr>
